0e5267c8fe5c19da1de99a5262d564fc362713d7
Angelo456yy <al8817515@gmail.com>
2024-03-09 12:34:55 -0600

    primer commit

---
+++ Corero/Corero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Correo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+            Console.WriteLine("Escribe tu nombre"); 
+            string nombre = Console.ReadLine();
+            Console.WriteLine("ahora tu apellido");
+            String apellido = Console.ReadLine();
+            Console.WriteLine("escribe tu dominio (gmail, hotmail, etc )");
+            String dominio = Console.ReadLine();
+            string A = "@";
+            string com = ".com";
+
+
+            Console.WriteLine("Tu correo es " + nombre+apellido+A+dominio+com);
+        }
+    }
+}
