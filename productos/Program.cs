e065566705c5ea3464abae140c0d9d16c81d16bc
Angelo456yy <al8817515@gmail.com>
2024-03-09 12:46:48 -0600

    hola

---
+++ productos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+            Console.WriteLine("ingrese el precio del primer productos");
+            string primer = Console.ReadLine();
+            Console.WriteLine("ingrese el precio del segundo productos");
+            string  segundo = Console.ReadLine();
+            Console.WriteLine("ingrese el precio del tercero  productos");
+            string tercero = Console.ReadLine();
+
+            int pri = 
+            int suma = primer + segundo + tercero;
+            
+
+
+        }
+    }
+}
